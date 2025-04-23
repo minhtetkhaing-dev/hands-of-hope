ee939bf48bdfbd519f7584718d7d2c453bff87f1
minhtetkhaing <minhtetkhaing.dev@gmail.com>
2025-04-23 09:31:51 +0900

    vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,17 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import apiRoutes from './routes/index';
+import apiRoutes from '../routes/index';
+import serverless from 'serverless-http';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
 
-// Middleware
 app.use(express.json());
-
 app.use('/api', apiRoutes);
 
-// Basic route
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Welcome to the Helping Hands API!' });
 });
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+export const handler = serverless(app);
